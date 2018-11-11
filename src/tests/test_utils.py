e3b4eaa86caa8f50b536c2b3fc83b4ed37d4bbee
elham_kh <elham.kheradmand@polymtl.ca>
2018-11-11 17:04:35 -0500

    added two test methods in the tests folder, one tests the csv reader and the other one tests the histogram computation

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,14 +1,16 @@
-from src.utils import get_split_line
+from src.utils import *
+import pytest
 
+# test the csv reader function
+def test_csv_read():
+    x, y = csv_reader('test-data.csv')
+    assert x[1][1] == 1.68, 'csv file read failed'
+    assert y[0][0] == 32, 'csv file read failed'
+    assert (float(x[0][0])+float(x[1][0]))/2 == 35.5, 'csv file read failed'
 
-def test_get_split_line():
-
-    test_line = 'I, like, Python'
-    split_line = get_split_line(test_line)
-
-    split_line[0] == 'I'
-    split_line[1] == 'like'
-    split_line[2] == 'Python'
-
-
-
+# test the histogram generator function
+def test_histogram():
+    x, y = csv_reader('test-data.csv')
+    counts_x, bin_edges_x, counts_y, bin_edges_y, column_width = calculate_histogram(x,y)
+    assert counts_x[0] == 2, 'histogram calculation failed'
+    assert bin_edges_y[1] == 2.3256, 'histogram calculation failed'
